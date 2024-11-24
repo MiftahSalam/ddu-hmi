a63c5cd9fa2c56a1b226ae986a1d37421e1eb280
miftah <salam.miftah@gmail.com>
2024-11-24 18:34:00 +0700

    enhance - add button handler click

--- javafx/src/main/java/skm/ddu/mlh/views/components/ChannelButton.java
+++ javafx/src/main/java/skm/ddu/mlh/views/components/ChannelButton.java
@@ -1,5 +1,7 @@
 package skm.ddu.mlh.views.components;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 
 public class ChannelButton extends Button {
@@ -15,5 +17,38 @@ public class ChannelButton extends Button {
     public ChannelButton(int number, ChannelButtonRole role) {
         chNum = number;
         chRole = role;
+
+        EventHandler<ActionEvent> click;
+        switch (chRole) {
+            case CH_NUMBER:
+                click = new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        // TODO: call setting page
+
+                        System.out.println("button ch: " + chNum + ", role: " + role.name());
+                    };
+                };
+                break;
+            case CH_NAME:
+                click = new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        // TODO: call info page
+                        System.out.println("button ch: " + chNum + ", role: " + role.name());
+                    };
+                };
+                break;
+            default:
+                click = new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("button ch: " + chNum + ", role: " + role.name());
+                    };
+                };
+                break;
+        }
+
+        setOnAction(click);
     }
 }
