7ea34a14792798688269778bbb3cc72ada8fb2c6
miftah <salam.miftah@gmail.com>
2024-11-26 13:10:42 +0700

    enahnce - add right bar panel

--- javafx/src/main/java/skm/ddu/mlh/views/layouts/HomePage.java
+++ javafx/src/main/java/skm/ddu/mlh/views/layouts/HomePage.java
@@ -1,16 +1,48 @@
 package skm.ddu.mlh.views.layouts;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import skm.ddu.mlh.views.panels.ChannelsPanel;
 
-public class HomePage extends VBox {
+public class HomePage extends HBox {
 
     public HomePage() {
         ChannelsPanel ch = new ChannelsPanel();
+        VBox rightBar = createRightBar();
 
-        setVgrow(ch, Priority.ALWAYS);
+        rightBar.prefHeightProperty().bind(ch.heightProperty().multiply(0.2));
+        rightBar.prefWidthProperty().bind(ch.widthProperty().multiply(0.2));
 
-        super.getChildren().add(ch);
+        setHgrow(ch, Priority.ALWAYS);
+        setHgrow(rightBar, Priority.SOMETIMES);
+
+        super.getChildren().addAll(ch, rightBar);
+    }
+
+    private VBox createRightBar() {
+        VBox boxBar = new VBox();
+        VBox boxButton = new VBox();
+
+        boxButton.prefHeightProperty().bind(boxBar.heightProperty().multiply(0.3));
+
+        Region spacer = new Region();
+        Button pageButton = new Button("Next");
+        Button exitButton = new Button("Shutdown");
+
+        pageButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        exitButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        VBox.setVgrow(spacer, Priority.ALWAYS);
+        VBox.setVgrow(pageButton, Priority.ALWAYS);
+        VBox.setVgrow(exitButton, Priority.ALWAYS);
+
+        boxButton.getChildren().addAll(pageButton, exitButton);
+
+        boxBar.getChildren().addAll(spacer, boxButton);
+
+        return boxBar;
     }
 }
