ad5b809765f0e71c0e569b9f2924b9421c739b82
miftah <salam.miftah@gmail.com>
2024-11-26 11:16:07 +0700

    enhance - create channel list column and label

--- javafx/src/main/java/skm/ddu/mlh/views/panels/ChannelsPanel.java
+++ javafx/src/main/java/skm/ddu/mlh/views/panels/ChannelsPanel.java
@@ -1,24 +1,93 @@
 package skm.ddu.mlh.views.panels;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import skm.ddu.mlh.views.components.ChannelRow;
 
 public class ChannelsPanel extends VBox {
     public ChannelsPanel() {
+        HBox label = generateChannelLabelRow();
+        HBox col1 = generateChannelRows1();
+        HBox col2 = generateChannelRows2();
+
+        setVgrow(label, Priority.ALWAYS);
+        setVgrow(col1, Priority.ALWAYS);
+        setVgrow(col2, Priority.ALWAYS);
+
+        super.getChildren().add(label);
+        super.getChildren().add(col1);
+        super.getChildren().add(col2);
+    }
+
+    private HBox generateChannelRows1() {
+        HBox box = new HBox();
+
         ChannelRow ch1 = new ChannelRow();
         ChannelRow ch2 = new ChannelRow();
-        ChannelRow ch3 = new ChannelRow();
-        ChannelRow ch4 = new ChannelRow();
 
-        setVgrow(ch1, Priority.ALWAYS);
-        setVgrow(ch2, Priority.ALWAYS);
-        setVgrow(ch3, Priority.ALWAYS);
-        setVgrow(ch4, Priority.ALWAYS);
+        HBox.setHgrow(ch1, Priority.ALWAYS);
+        HBox.setHgrow(ch2, Priority.ALWAYS);
 
-        super.getChildren().add(0, ch1);
-        super.getChildren().add(1, ch2);
-        super.getChildren().add(2, ch3);
-        super.getChildren().add(3, ch4);
+        box.getChildren().add(ch1);
+        box.getChildren().add(ch2);
+
+        return box;
+    }
+
+    private HBox generateChannelRows2() {
+        HBox box = new HBox();
+
+        ChannelRow ch1 = new ChannelRow();
+        ChannelRow ch2 = new ChannelRow();
+
+        HBox.setHgrow(ch1, Priority.ALWAYS);
+        HBox.setHgrow(ch2, Priority.ALWAYS);
+
+        box.getChildren().add(ch1);
+        box.getChildren().add(ch2);
+
+        return box;
+    }
+
+    private HBox generateChannelLabelRow() {
+        HBox box = new HBox();
+        HBox box1 = generateLabelRow();
+        HBox box2 = generateLabelRow();
+
+        HBox.setHgrow(box1, Priority.ALWAYS);
+        HBox.setHgrow(box2, Priority.ALWAYS);
+
+        box.getChildren().addAll(box1, box2);
+
+        return box;
+    }
+
+    private HBox generateLabelRow() {
+        HBox box = new HBox();
+
+        Label chNum = new Label();
+        chNum.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        chNum.setText("CHANNEL");
+        chNum.setAlignment(Pos.CENTER);
+        HBox.setHgrow(chNum, Priority.ALWAYS);
+
+        Label chName = new Label();
+        chName.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        chName.setText("SENSOR");
+        chName.setAlignment(Pos.CENTER);
+        HBox.setHgrow(chName, Priority.ALWAYS);
+
+        Label chVal = new Label();
+        chVal.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        chVal.setText("VALUE");
+        chVal.setAlignment(Pos.CENTER);
+        HBox.setHgrow(chVal, Priority.ALWAYS);
+
+        box.getChildren().addAll(chNum, chName, chVal);
+
+        return box;
     }
 }
