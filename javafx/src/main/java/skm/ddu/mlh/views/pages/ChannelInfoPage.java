bfac7565b71b83aab89cd2b1a3ec0b67824592d4
miftah <salam.miftah@gmail.com>
2024-11-27 18:42:42 +0700

    enhance - create channel info page

---
+++ javafx/src/main/java/skm/ddu/mlh/views/pages/ChannelInfoPage.java
@@ -0,0 +1,17 @@
+package skm.ddu.mlh.views.pages;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class ChannelInfoPage {
+
+    @FXML
+    private Button buttonOk;
+
+    @FXML
+    void onClicked(MouseEvent event) {
+
+    }
+
+}
