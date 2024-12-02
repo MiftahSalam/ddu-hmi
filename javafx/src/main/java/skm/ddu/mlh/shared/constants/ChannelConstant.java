d3e4ba5071138e91111449cc5f06983fb6f017ea
miftah <salam.miftah@gmail.com>
2024-12-02 18:18:38 +0700

    enhance - create channel settings constant

--- javafx/src/main/java/skm/ddu/mlh/shared/constants/ChannelConstant.java
+++ javafx/src/main/java/skm/ddu/mlh/shared/constants/ChannelConstant.java
@@ -15,6 +15,134 @@ public class ChannelConstant {
         OUT
     }
 
+    public enum CH_JENIS_NMEA {
+        NONE,
+        AG,
+        AI,
+        AP,
+        DC,
+        CR,
+        CS,
+        CT,
+        CV,
+        CX,
+        DE,
+        DF,
+        EC,
+        EP,
+        ER,
+        GL,
+        GN,
+        GP,
+        GV,
+        HC,
+        HE,
+        HN,
+        II,
+        IN,
+        LA,
+        LC,
+        OM,
+        P,
+        RA,
+        SD,
+        SN,
+        SS,
+        TI,
+        TR,
+        VD,
+        VM,
+        VW,
+        YX,
+        ZA,
+        ZC,
+        ZQ,
+        WI,
+    }
+
+    public enum CH_NMEA_VALUE {
+        NONE,
+        DBT,
+        DPT,
+        FPD,
+        GGA,
+        GLL,
+        HDG,
+        HDT,
+        HSC,
+        MWD,
+        MWV,
+        SN,
+        TTM,
+        VBW,
+        VDM,
+        VDR,
+        VHW,
+        VTG
+    }
+
+    public enum CH_SENSOR_NAME {
+        NONE,
+        AIS,
+        ARPA,
+        Echo_Sounder_1,
+        Echo_Sounder_2,
+        Echo_Sounder_3,
+        GPS_1,
+        GPS_2,
+        GPS_3,
+        Gyro_Compas_1,
+        Gyro_Compas_2,
+        Gyro_Compas_3,
+        Gyro_Vertical_1,
+        Gyro_Vertical_2,
+        Gyro_Vertical_3,
+        Meteo,
+        Speedlog_1,
+        Speedlog_2,
+        Speedlog_3,
+        Wind_Direction_1,
+        Wind_Direction_2,
+        Wind_Direction_3,
+    }
+
+    public enum CH_SERIAL_BAUDRATE {
+        B_2400,
+        B_4800,
+        B_9600,
+        B_19200,
+        B_38400,
+        B_56800,
+        B_115200,
+    }
+
+    public enum CH_SERIAL_DATABITS {
+        NONE,
+        DATA_5,
+        DATA_6,
+        DATA_7,
+        DATA_8,
+    }
+
+    public enum CH_SERIAL_PARITY {
+        NONE,
+        ODD,
+        EVEN,
+    }
+
+    public enum CH_SERIAL_FLOW_CONTROL {
+        NONE,
+        OFF,
+        HARDWARE,
+        XON_XOFF,
+    }
+
+    public enum CH_SERIAL_STOPBITS {
+        NONE,
+        STOP_1,
+        STOP_2,
+    }
+
     public static final Map<Integer, CH_IO_FUNCTION> CH_IO_MAP = new HashMap<>();
 
     static {
