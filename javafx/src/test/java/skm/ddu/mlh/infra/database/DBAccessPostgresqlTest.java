cf042381bbe9dd5752eeedc308982da9c40b2fc0
miftah <salam.miftah@gmail.com>
2024-12-05 12:23:28 +0700

    adjust - refactor unit test

--- javafx/src/test/java/skm/ddu/mlh/infra/database/DBAccessPostgresqlTest.java
+++ javafx/src/test/java/skm/ddu/mlh/infra/database/DBAccessPostgresqlTest.java
@@ -4,17 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import skm.ddu.mlh.shared.configs.DatabaseConfig;
 
 public class DBAccessPostgresqlTest {
-    @Test
-    void testConnection() {
+    private static DBAccessPostgresql dbAccessPostgresql;
+
+    @BeforeAll
+    static void setup() {
         assertDoesNotThrow(() -> {
             DatabaseConfig cfg = DatabaseConfig.getInstance("localhost:5432:postgres:fisikawangarut:skmddu_2014");
 
-            DBAccessPostgresql dbAccessPostgresql = DBAccessPostgresql.getInstance(cfg);
+            dbAccessPostgresql = DBAccessPostgresql.getInstance(cfg);
             assertNotNull(dbAccessPostgresql);
         });
     }
@@ -22,11 +25,6 @@ public class DBAccessPostgresqlTest {
     @Test
     void testExecuteQuery() {
         assertDoesNotThrow(() -> {
-            DatabaseConfig cfg = DatabaseConfig.getInstance("localhost:5432:postgres:fisikawangarut:skmddu_2014");
-
-            DBAccessPostgresql dbAccessPostgresql = DBAccessPostgresql.getInstance(cfg);
-            assertNotNull(dbAccessPostgresql);
-
             assertEquals(26, dbAccessPostgresql.executeQuery("SELECT * FROM ddu_ch"));
         });
     }
